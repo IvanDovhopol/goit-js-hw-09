445e5713458e349a210866731d90ace77f21c6f7
Ivan <104319381+IvanDovhopol@users.noreply.github.com>
2022-09-30 17:36:30 +0300

    create color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,3 +2,37 @@ const refs = {
   startBtn: document.querySelector('[data-start]'),
   stopBtn: document.querySelector('[data-stop]'),
 };
+
+let intevalId = null;
+
+refs.startBtn.addEventListener('click', () => {
+  intevalId = setInterval(() => {
+    addBgColorForBody();
+  }, 1000);
+
+  startTimer();
+  addBgColorForBody();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  intevalId = clearInterval(intevalId);
+  stopTimer();
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function startTimer() {
+  refs.stopBtn.disabled = false;
+  refs.startBtn.disabled = true;
+}
+
+function stopTimer() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
+
+function addBgColorForBody() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
